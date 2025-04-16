6505f437fb509870d601ca6cff5bf196f5422717
Charles Ganza <charlesganza@gmail.com>
2025-04-16 15:15:56 +0200

    progress

--- kwik/src/main/kotlin/com/isakaro/kwik/inputfields/KwikTextField.kt
+++ kwik/src/main/kotlin/com/isakaro/kwik/inputfields/KwikTextField.kt
@@ -298,7 +298,7 @@ fun KwikTextField(
             textStyle = textStyle,
             visualTransformation = if(visualTransformation is PasswordVisualTransformation) {
                 if(passwordVisible) VisualTransformation.None else PasswordVisualTransformation()
-            } else VisualTransformation.None,
+            } else visualTransformation,
             modifier = Modifier
                 .fillMaxWidth()
                 .heightIn(if(isBigTextField) 150.dp else 65.dp)
